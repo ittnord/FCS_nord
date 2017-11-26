398dfb26982e7b27dd23c9eabb292d8a2deec3ef
lsyu <powt81lsyu@gmail.com>
2017-11-26 10:19:28 +0300

    Выбор абилок

---
+++ Assets/Scripts/AbilitiesStorage.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using FCS;
+using UnityEngine;
+
+public class AbilitiesStorage : Singleton<AbilitiesStorage>
+{
+    private readonly HashSet<Abilities> selectedAbilities = new HashSet<Abilities>();
+
+    public event Action<bool> AvailableChanged;
+
+    public bool CanStart => selectedAbilities.Count == 3;
+
+    public List<Abilities> SelectedAbilities
+    {
+        get
+        {
+            var result = new List<Abilities>();
+            result.Add(Abilities.DefaultAbility);
+            result.AddRange(selectedAbilities);
+            return result;
+        }
+    }
+
+    public void ChangeState(Abilities abilities)
+    {
+        if (selectedAbilities.Contains(abilities))
+        {
+            selectedAbilities.Remove(abilities);
+        }
+        else
+        {
+            if (selectedAbilities.Count >= 3)
+            {
+                return;
+            }
+
+            selectedAbilities.Add(abilities);
+        }
+
+        if (AvailableChanged != null)
+        {
+            AvailableChanged(CanStart);
+        }
+    }
+}
---
+++ Assets/Scripts/UI/SelectAbilitiesPanel.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using FCS;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace FCS
+{
+    public class SelectAbilitiesPanel : MonoBehaviour
+    {
+        [SerializeField] private Button _explosionButton;
+        [SerializeField] private Button _fireballButton;
+        [SerializeField] private Button _forcePushButton;
+        [SerializeField] private Button _hookButton;
+        [SerializeField] private Button _invisibilityButton;
+        [SerializeField] private Button _lifeDrainButton;
+        [SerializeField] private Button _shieldButton;
+        [SerializeField] private Button _swapButton;
+        [SerializeField] private Button _teleportButton;
+        
+
+        private void ChangeState(Button button, Abilities abilities)
+        {
+            var selectedAbilities = AbilitiesStorage.Instance.SelectedAbilities;
+            AbilitiesStorage.Instance.ChangeState(abilities);
+            if (selectedAbilities.Contains(abilities))
+            {
+                SetSelected(button, false);
+            }
+            else
+            {
+                if (selectedAbilities.Count >= 3)
+                {
+                    return;
+                }
+                
+                SetSelected(button, true);
+            }
+        }
+
+        public void SetSelected(Button button, bool state)
+        {
+
+        }
+
+        public void OnAddExplosion()
+        {
+            ChangeState(_explosionButton, Abilities.Explosion);
+        }
+
+        public void OnAddFireball()
+        {
+            ChangeState(_fireballButton, Abilities.Fireball);
+        }
+
+        public void OnAddForcePush()
+        {
+            ChangeState(_forcePushButton, Abilities.ForcePush);
+        }
+
+        public void OnAddHook()
+        {
+            ChangeState(_hookButton, Abilities.Hook);
+        }
+
+        public void OnAddInvisibility()
+        {
+            ChangeState(_invisibilityButton, Abilities.Invisibility);
+        }
+
+        public void OnAddLifeDrain()
+        {
+            ChangeState(_lifeDrainButton, Abilities.LifeDrain);
+        }
+
+        public void OnAddShield()
+        {
+            ChangeState(_shieldButton, Abilities.Shield);
+        }
+
+        public void OnAddSwap()
+        {
+            ChangeState(_swapButton, Abilities.Swap);
+        }
+
+        public void OnAddTeleport()
+        {
+            ChangeState(_teleportButton, Abilities.Teleport);
+        }
+    }
+}
